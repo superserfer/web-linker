1154ffb4fd71be6a17ce75dafa0cc42cb4b7ef75
Linus Ackermann <linus.ackermann@hispeed.ch>
2021-03-08 18:29:17 +0100

    add forgot password component & implement user.service.ts

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+// @ts-ignore
+import server from '../../environments/server.json';
+import {User} from '../models/user';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getByUsername(username: string): Observable<User>{
+    return this.httpClient.get<User>(server.address + '/byUsername/' + username);
+  }
+
+  public getById(id: string): Observable<User>{
+    return this.httpClient.get<User>(server.address + '/byId/' + id);
+  }
+
+  public createUser(newUser: User): Observable<User> {
+    return this.httpClient.post<User>(server.address + '/', newUser);
+  }
+
+  public updateUser(updatedUser: User): Observable<User> {
+    return this.httpClient.put<User>(server.address + '/', updatedUser);
+  }
+}
