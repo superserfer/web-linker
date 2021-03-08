5ac1ed095d16f4283fc61e9ffaf72cda17c387a7
Linus Ackermann <linus.ackermann@hispeed.ch>
2021-03-08 18:56:13 +0100

    correct path for user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -13,18 +13,18 @@ export class UserService {
   constructor(private httpClient: HttpClient) { }
 
   public getByUsername(username: string): Observable<User>{
-    return this.httpClient.get<User>(server.address + '/byUsername/' + username);
+    return this.httpClient.get<User>(server.address + '/user/byUsername/' + username);
   }
 
   public getById(id: string): Observable<User>{
-    return this.httpClient.get<User>(server.address + '/byId/' + id);
+    return this.httpClient.get<User>(server.address + '/user/byId/' + id);
   }
 
   public createUser(newUser: User): Observable<User> {
-    return this.httpClient.post<User>(server.address + '/', newUser);
+    return this.httpClient.post<User>(server.address + '/user/', newUser);
   }
 
   public updateUser(updatedUser: User): Observable<User> {
-    return this.httpClient.put<User>(server.address + '/', updatedUser);
+    return this.httpClient.put<User>(server.address + '/user/', updatedUser);
   }
 }
